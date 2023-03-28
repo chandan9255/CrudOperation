be2a003dc60cc3155ff438a5a50a4ec3d6e57022
chandan9050 <ck981295@gmail.com>
2023-03-28 21:00:50 +0530

    Project created

--- MVC5App/Controllers/HomeController.cs
+++ MVC5App/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MVC5App.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +10,79 @@ namespace MVC5App.Controllers
 {
     public class HomeController : Controller
     {
+        StudentEntities1 db = new StudentEntities1();
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public JsonResult GetStudentList()
         {
-            ViewBag.Message = "Your application description page.";
+            List<StudentModel> StudList = db.StudentDatas.Select(x => new StudentModel
+            {
+                StudentId = x.StudentId,
+                Name=x.Name,
+                Roll_NO=x.Roll_NO,
+                Department=x.Department
+            }).ToList();
 
-            return View();
+            return Json(StudList, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult Contact()
+        public JsonResult GetStudentById(int StudentId)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            StudentData model = db.StudentDatas.Where(x => x.StudentId == StudentId).SingleOrDefault();
+            string value = string.Empty;
+            value = JsonConvert.SerializeObject(model, Formatting.Indented, new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
+            return Json(value, JsonRequestBehavior.AllowGet);
         }
+
+        public JsonResult SaveDataInDatabase(StudentModel model)
+        {
+            var result = false;
+            try
+            {
+                if (model.StudentId > 0)
+                {
+                    StudentData Stu = db.StudentDatas.SingleOrDefault(x =>x.StudentId == model.StudentId);
+                    Stu.Name = model.Name;
+                    Stu.Roll_NO = model.Roll_NO;
+                    Stu.Department = model.Department;
+                    db.SaveChanges();
+                    result = true;
+                }
+                else
+                {
+                    StudentData Stu = new StudentData();
+                    Stu.Name = model.Name;
+                    Stu.Roll_NO = model.Roll_NO;
+                    Stu.Department = model.Department;
+                    db.StudentDatas.Add(Stu);
+                    db.SaveChanges();
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult DeleteStudentRecord(int StudentId)
+        {
+            bool result = false;
+            StudentData Stu = db.StudentDatas.SingleOrDefault(x => x.StudentId == StudentId);
+            if (Stu != null)
+            {
+                db.StudentDatas.Remove(Stu);
+                db.SaveChanges();
+                result = true;
+            }
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
---
+++ MVC5App/Models/StudentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5App.Models
+{
+    public class StudentModel
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Roll_NO { get; set; }
+        public string Department { get; set; }
+    }
+}
